d32bcf6637117982e09d8310064f2f483c7a5d41
bilgejava6 <bilgejava6@gmail.com>
2025-03-08 10:56:43 +0300

    Initial commit

---
+++ src/main/java/com/muhammet/springbootprojecttest/SpringBootProjectTestApplication.java
@@ -0,0 +1,13 @@
+package com.muhammet.springbootprojecttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjectTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootProjectTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/muhammet/springbootprojecttest/SpringBootProjectTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muhammet.springbootprojecttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProjectTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
